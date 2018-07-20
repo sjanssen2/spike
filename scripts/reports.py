d2b2da2d9fc6b95cacdcb4c60d69488e3513eb52
Stefan Janssen <stefan.m.janssen@gmail.com>
2018-07-20 15:36:39 +0200

    added title plus file info

--- scripts/reports.py
+++ scripts/reports.py
@@ -1,12 +1,13 @@
-from os.path import join, exists, dirname
+from os.path import join, exists, dirname, basename
 from os import makedirs
 import sys
 import pandas as pd
 from glob import glob
 import seaborn as sns
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import stats
+import matplotlib.pyplot as plt
+plt.switch_backend('Agg')
 
 
 def report_undertermined_filesizes(fp_filesizes, fp_output, fp_error,
@@ -46,14 +47,15 @@ def report_undertermined_filesizes(fp_filesizes, fp_output, fp_error,
         kde=False, rug=True, color="green", label='this run')
     _ = ax.set_ylabel('number of files')
     _ = ax.set_xlabel('file-size in GB')
+    ax.set_title('run %s' % basename(fp_filesizes)[:-4])
     ax.legend()
 
     # raise error if current run contains surprisingly large undetermined
     # filesize
     if pd_sizes[(pd_sizes['isme'] == np.True_) &
                 (pd_sizes['status'] == 'unknown')]['z-score'].max() > zscorethreshold:
-        fp_plot = ''
-        fig.savefig(fp_plot, bbox_inches='tight')
+        ax.set_title('ERROR: %s' % ax.get_title())
+        fig.savefig(fp_error, bbox_inches='tight')
         raise ValueError(
             ("Compared to known historic runs, your run contains surprisingly "
              "(z-score > %f) large file(s) of undetermined reads. You will find"
@@ -64,6 +66,6 @@ def report_undertermined_filesizes(fp_filesizes, fp_output, fp_error,
              "3. for future automatic considerations, mark file status (3rd "
              "column) as 'bad' if you have decided to abort processing due to"
              " too low quality (z-score kind of averages about known values)."
-             ) % (zscorethreshold, fp_plot, fp_filesizes))
+             ) % (zscorethreshold, fp_error, fp_filesizes))
     else:
         fig.savefig(fp_output, bbox_inches='tight')
