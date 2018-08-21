deb6cf16f4d8e9774a658f72a3d0f13c22cb9352
Stefan Janssen <stefan.m.janssen@gmail.com>
2018-08-21 10:23:49 +0200

    fixed sampel names

--- scripts/tests/test_parse_samplesheet.py
+++ scripts/tests/test_parse_samplesheet.py
@@ -102,11 +102,11 @@ class ParseSamplesheetTests(TestCase):
             'Maus_Hauer/291C',
             'Maus_Hauer/291T'],
         "Agilent_SureSelect_Human_AllExon_V6_r2.hGRC37.Covered.bed": [
-            'ALL_family_LB/LB_4_2',
-            'ALL_family_LB/LB_4_3',
-            'ALL_family_LB/LB_5_1',
-            'ALL_family_LB/LB_5_2',
-            'ALL_family_LB/LB_5_3']}
+            'ALL_family_LB/LB_4-2',
+            'ALL_family_LB/LB_4-3',
+            'ALL_family_LB/LB_5-1',
+            'ALL_family_LB/LB_5-2',
+            'ALL_family_LB/LB_5-3']}
 
         for entity in self.samplesheets[~self.samplesheets['Sample_Project'].isin(['AG_Remke'])]['fastq-prefix'].unique():
             obs = get_reference_exometrack(entity, self.samplesheets, self.config)
@@ -115,7 +115,7 @@ class ParseSamplesheetTests(TestCase):
     def test_get_bwa_mem_header(self):
         self.assertIn('SureSelectXTV5plusUTRautomated', get_bwa_mem_header('Alps/ALPS_64', self.samplesheets, self.config))
         self.assertIn('SureSelectXTmouse', get_bwa_mem_header('Maus_Hauer/mMicro297_T', self.samplesheets, self.config))
-        self.assertIn('SureSelectHumanAllExonV6r2', get_bwa_mem_header('ALL_family_LB/LB_5_3', self.samplesheets, self.config))
+        self.assertIn('SureSelectHumanAllExonV6r2', get_bwa_mem_header('ALL_family_LB/LB_5-3', self.samplesheets, self.config))
 
 
 if __name__ == '__main__':
