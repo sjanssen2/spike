5381b90eab926d8550b147ce87d07ee27fd6848e
Stefan Janssen <stefan.m.janssen@gmail.com>
2018-08-21 07:49:41 +0200

    fixed old test

--- scripts/tests/test_parse_samplesheet.py
+++ scripts/tests/test_parse_samplesheet.py
@@ -5,7 +5,10 @@ from unittest import TestCase, main
 from scripts.parse_samplesheet import *
 
 class ParseSamplesheetTests(TestCase):
-    fp_samplesheet = 'tests/data/180614_SN737_0438_BCC7MCACXX_ukd.csv'
+    samplesheets = None
+
+    def setUp(self):
+        self.samplesheets = get_global_samplesheets('tests/data/')
 
     def test_get_role(self):
         config = {'dirs': {'prefix': './',
@@ -13,24 +16,24 @@ class ParseSamplesheetTests(TestCase):
                            'samplesheets': 'data'}}
         ukd_project = 'Alpss'
         with self.assertRaisesRegex(ValueError,
-                                    ('Could not find an UKD project with name "%s". Available projects are:\n\t%s\n' % (ukd_project, '\n\t'.join(sorted(['AG_Remke', 'Alps', 'Fischer_Geron', 'Keimbahn', 'Maus_Hauer', 'Mouse_Spain']))))):
-            get_role(ukd_project, None, None, config)
+                                    ('Could not find an UKD project with name "%s". Available projects are:\n\t%s\n' % (ukd_project, '\n\t'.join(sorted(['AG_Remke', 'Alps', 'Fischer_Geron', 'Keimbahn', 'Maus_Hauer']))))):
+            get_role(ukd_project, None, None, self.samplesheets)
 
         ukd_project = 'Alps'
         ukd_entity_id = 'ALPS_6x'
         with self.assertRaisesRegex(ValueError,
                                     ('Could not find an UKD entity group with name "%s". Available entities for projects "%s" are:\n\t%s\n' % (ukd_entity_id, ukd_project, '\n\t'.join(sorted(['ALPS_64', 'ALPS_65', 'ALPS_66']))))):
-            get_role(ukd_project, ukd_entity_id, None, config)
+            get_role(ukd_project, ukd_entity_id, None, self.samplesheets)
 
         ukd_entity_id = 'ALPS_66'
         ukd_entity_role = 'grandma'
         with self.assertRaisesRegex(ValueError,
                                     ('Could not find a role "%s" for UKD entity group with name "%s". Available roles are:\n\t%s\n' % (ukd_entity_role, ukd_entity_id, '\n\t'.join(sorted(['father', 'mother', 'patient']))))):
-            get_role(ukd_project, ukd_entity_id, ukd_entity_role, config)
+            get_role(ukd_project, ukd_entity_id, ukd_entity_role, self.samplesheets)
 
         ukd_entity_role = 'father'
-        exp = '180614_SN737_0438_BCC7MCACXX/Alps/ALPS_66_a'
-        obs = get_role(ukd_project, ukd_entity_id, ukd_entity_role, config)
+        exp = 'Alps/ALPS_66_a'
+        obs = get_role(ukd_project, ukd_entity_id, ukd_entity_role, self.samplesheets)
         self.assertEqual(exp, obs)
 
 
