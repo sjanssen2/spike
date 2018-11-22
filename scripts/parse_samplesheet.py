14f6f2d9cb945d8498481bb7a90c51dd2731ef35
Stefan Janssen <stefan.m.janssen@gmail.com>
2018-11-22 08:08:24 +0100

    merging sample and header information

--- scripts/parse_samplesheet.py
+++ scripts/parse_samplesheet.py
@@ -28,7 +28,9 @@ def parse_samplesheet(fp_samplesheet):
     header = pd.read_csv(fp_samplesheet, sep=",", nrows=row_reads-2).dropna(axis=1, how="all").dropna(axis=0, how="all")
     header = header.set_index(header.columns[0])
     header.index = list(map(lambda x: 'header_%s' % x, header.index))
-    #return header.T
+    header.dropna(axis=0, how="any", inplace=True)
+    header = header.T.reset_index()
+    del header['index']
 
     # a xxx iteration parses sample information via pandas
     ss = pd.read_csv(fp_samplesheet, sep=",", skiprows=row_sampleinformation, dtype={'Sample_Name': str, 'Sample_ID': str, 'spike_entity_id': str})
@@ -65,6 +67,12 @@ def parse_samplesheet(fp_samplesheet):
 
     # remove samples that are marked to be ignored
     ss = ss[pd.isnull(ss['spike_ignore_sample'])]
+
+    # merge with header information
+    if not all([c not in ss.columns for c in header.columns]):
+        raise ValueError("Header name conflicts with sample column in '%s'." % fp_samplesheet)
+    for c in header.columns:
+        ss[c] = header[c].iloc[0]
 
     return ss
 
